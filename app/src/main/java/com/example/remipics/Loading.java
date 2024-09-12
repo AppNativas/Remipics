73ace3fa25a3ad8e86f10d77a9ec229bc4d9f3e4
Manuel <garciahildo@gmail.com>
2024-09-11 23:32:18 -0500

    Init

---
+++ app/src/main/java/com/example/remipics/Loading.java
@@ -0,0 +1,38 @@
+package com.example.remipics;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Timer;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Intent intent;
+        new Timer().schedule(() + {
+            Intent intent = new Intent(Loading.class, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+    }, 3000);
+    }
+}
